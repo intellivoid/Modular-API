447b289476f53d31b9db8a5740aa001e55ef69e7
Narrakas <zixing.narrakas1996@gmail.com>
2019-01-30 18:10:22 -0300

    Created ModularAPI.php and configuration.ini

---
+++ src/ModularAPI/ModularAPI.php
@@ -0,0 +1,29 @@
+<?php
+
+    namespace ModularAPI;
+
+    /**
+     * Main AutoLoader for ModularAPI
+     *
+     * Class ModularAPI
+     * @package ModularAPI
+     */
+    class ModularAPI
+    {
+        /**
+         * Constructs ModularAPI Library
+         *
+         * ModularAPI constructor.
+         * @param string $ConfigurationFile
+         * @param bool $EstablishDatabaseConnection
+         */
+        public function __construct(string $ConfigurationFile = 'default', bool $EstablishDatabaseConnection = true)
+        {
+            if($ConfigurationFile == 'default')
+            {
+                $ConfigurationFile = __DIR__ . DIRECTORY_SEPARATOR . 'configuration.ini';
+            }
+
+            $this->Configuration = parse_ini_file($ConfigurationFile, true);
+        }
+    }
