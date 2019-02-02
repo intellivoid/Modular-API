7bc1c59170cfb91c86deb0c4a2260adbc70ed2c6
Narrakas <zixing.narrakas1996@gmail.com>
2019-02-02 01:28:21 -0300

    Added toArray() and fromArray() to AccessKey.php

--- src/ModularAPI/Objects/AccessKey.php
+++ src/ModularAPI/Objects/AccessKey.php
@@ -2,6 +2,7 @@
 
     namespace ModularAPI\Objects;
     use ModularAPI\Abstracts\AccessKeyStatus;
+    use ModularAPI\Objects\AccessKey\Analytics;
     use ModularAPI\Objects\AccessKey\Usage;
 
     /**
@@ -44,4 +45,65 @@
          * @var Usage
          */
         public $Usage;
+
+        /**
+         * The analytics data for this access key
+         *
+         * @var Analytics
+         */
+        public $Analytics;
+
+        /**
+         * Creates an array from the object
+         *
+         * @return array
+         */
+        public function toArray(): array
+        {
+            return array(
+                'id' => $this->ID,
+                'public_id' => $this->PublicKey,
+                'state' => $this->State,
+                'usage' => $this->Usage->toArray(),
+                'analytics' => $this->Analytics->toArray()
+            );
+        }
+
+        /**
+         * Creates an object from array
+         *
+         * @param array $data
+         * @return AccessKey
+         */
+        public static function fromArray(array $data): AccessKey
+        {
+            $AccessKeyObject = new AccessKey();
+
+            if(isset($data['id']))
+            {
+                $AccessKeyObject->ID = (int)$data['id'];
+            }
+
+            if(isset($data['public_id']))
+            {
+                $AccessKeyObject->PublicID = (string)$data['public_id'];
+            }
+
+            if(isset($data['state']))
+            {
+                $AccessKeyObject->State = (int)$data['state'];
+            }
+
+            if(isset($data['usage']))
+            {
+                $AccessKeyObject->Usage = Usage::fromArray($data['usage']);
+            }
+
+            if(isset($data['analytics']))
+            {
+                $AccessKeyObject->Analytics = Analytics::fromArray($data['analytics']);
+            }
+
+            return $AccessKeyObject;
+        }
     }
