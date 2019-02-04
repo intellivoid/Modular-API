0c31ef4fd0e3c2a04876c09b8affd4773068f9c5
Narrakas <zixing.narrakas1996@gmail.com>
2019-02-03 22:59:27 -0300

    Added toArray() and fromArray() in Policies.php

--- src/ModularAPI/Objects/Policies.php
+++ src/ModularAPI/Objects/Policies.php
@@ -21,4 +21,48 @@
          * @var bool
          */
         public $ForceCertificate;
+
+        /**
+         * Converts object to array
+         *
+         * @return array
+         */
+        public function toArray(): array
+        {
+            return array(
+                'AUTHENTICATION_REQUIRED' => $this->AuthenticatedRequired,
+                'FORCE_CERTIFICATE' => $this->ForceCertificate
+            );
+        }
+
+        /**
+         * Creates the object from an array
+         * 
+         * @param array $data
+         * @return Policies
+         */
+        public static function fromArray(array $data): Policies
+        {
+            $PoliciesObject = new Policies();
+        
+            if(isset($data['AUTHENTICATION_REQUIRED']))
+            {
+                $PoliciesObject->AuthenticatedRequired = (bool)$data['AUTHENTICATION_REQUIRED'];
+            }
+            else
+            {
+                $PoliciesObject->AuthenticatedRequired = false;
+            }
+            
+            if(isset($data['FORCE_CERTIFICATE']))
+            {
+                $PoliciesObject->ForceCertificate = (bool)$data['FORCE_CERTIFICATE'];
+            }
+            else
+            {
+                $PoliciesObject->ForceCertificate = false;
+            }
+        
+            return $PoliciesObject;
+        }
     }
