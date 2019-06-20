ec6e8e88105bda7305bb36f38cc98c1a47abb2ba
Narrakas <netkas@users.noreply.github.com>
2019-06-20 19:41:26 -0400

    Minor correction

--- src/ModularAPI/AutoConfig.php
+++ src/ModularAPI/AutoConfig.php
@@ -3,7 +3,6 @@
     use acm\acm;
     use acm\Objects\Schema;
 
-    include_once(__DIR__ . DIRECTORY_SEPARATOR . 'acm' . DIRECTORY_SEPARATOR . 'acm.php');
     $acm = new acm(__DIR__, 'ModularAPI');
 
     $DatabaseSchema = new Schema();
